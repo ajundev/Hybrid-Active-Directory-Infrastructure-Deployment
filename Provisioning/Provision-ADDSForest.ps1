c177aebd6db23cf6a758f565420d9edc7f921231
Amaechi Oghor <aoghor@wgu.edu>
2025-09-10 23:15:38 -0500

    Modified

--- Provisioning/Provision-ADDSForest.ps1
+++ Provisioning/Provision-ADDSForest.ps1
@@ -2,15 +2,17 @@
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 #Promote to Domain Controller for a New Forest
+Import-Module ADDSDeployment
 Install-ADDSForest `
--DomainName "corp.local" `          # Your domain FQDN
--DomainNetbiosName "CORP" `         # NetBIOS short name
--DomainMode "Win2022" `             # Windows Version
+-DomainName "" `                    # Your domain FQDN
+-DomainNetbiosName "" `             # NetBIOS short name
+-DomainMode "Win2022" `             # Version
+-ForestMode "Win2022" `             # Version
 -CreateDnsDelegation:$false `       # No delegation
 -DatabasePath "C:\Windows\NTDS" `   # AD DS database path
 -LogPath "C:\Windows\NTDS" `        # AD DS logs
 -SysvolPath "C:\Windows\SYSVOL" `   # SYSVOL share
 -InstallDns:$true `                 # Install DNS role
--SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssw0rd!" -AsPlainText -Force) # DSRM password
--NoRebootOnCompletion:$false `      # Will reboot when done
+-SafeModeAdministratorPassword (Read-Host "Enter DSRM password" -AsSecureString)    # DSRM password
+-NoRebootOnCompletion:$true `       # Will reboot when done
 -Force:$true                        # Skip confirmation
